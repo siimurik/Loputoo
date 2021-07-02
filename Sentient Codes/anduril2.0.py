424799ec19a12bb41a599cf5b1173c90b025b2e3
siimurik <82040815+siimurik@users.noreply.github.com>
2021-07-02 10:34:20 +0300

    Add files via upload

--- Sentient Codes/anduril2.0.py
+++ Sentient Codes/anduril2.0.py
@@ -25,14 +25,29 @@ alpha   = -np.arctan(yk/xk)
 beta    = np.arcsin(zk)
 gamma   = np.radians(-11.032961419914068) #
 delta   = -np.arcsin(hz)
-iota    = np.arctan(hy/hx)
 eta     = np.pi/2
-#print(np.degrees(alpha))
-#print(np.degrees(beta))
-#print(np.degrees(gamma))
-#print(np.degrees(delta))
-#print(np.degrees(iota))
-#print(np.degrees(eta))
+iota    = np.arctan(hy/hx) + eta
+#print(np.degrees(alpha),np.degrees(beta),np.degrees(gamma),
+#        np.degrees(delta),np.degrees(iota),np.degrees(eta))
+
+def bill_turner(x,y,z):
+    rx = np.radians(x)
+    ry = np.radians(y)
+    rz = np.radians(z)
+    x_turn = np.array([[1,          0,                  0],
+                       [0,      np.cos(rx),     -np.sin(rx)],
+                       [0,      np.sin(rx),      np.cos(rx)]])
+    v001 = np.array([0.0, 0.0, -1.0])
+    a = x_turn @ v001.T
+    y_turn = np.array([ [np.cos(ry),      0,      np.sin(ry)],
+                        [0,               1,          0],
+                        [-np.sin(ry),     0,      np.cos(ry)]])
+    b = y_turn @ a.T
+    z_turn = np.array([ [np.cos(rz),    -np.sin(rz),        0],
+                        [np.sin(rz),     np.cos(rz),        0],
+                        [     0,             0,             1]])
+    c = z_turn @ b.T
+    return c
 
 # rotation of axis (6 times)
 def will_turner(x,y,z):
@@ -63,11 +78,11 @@ def will_turner(x,y,z):
                     [0,                 0,          1]])
     i = i1 @ d
     # 6. turn - z-axis
-    e1 = np.array([ [np.cos(eta),  -np.sin(eta),    0],
-                    [np.sin(eta),   np.cos(eta),    0],
-                    [0,                 0,          1]])
-    e = e1 @ i
-    vec = np.array([(e[0])[0], (e[1])[0], (e[2])[0]])
+#    e1 = np.array([ [np.cos(eta),  -np.sin(eta),    0],
+#                    [np.sin(eta),   np.cos(eta),    0],
+#                    [0,                 0,          1]])
+#    e = e1 @ i
+    vec = np.array([(i[0])[0], (i[1])[0], (i[2])[0]])
     return vec
 
 # RZ to AZ doesnt work!!!
@@ -90,22 +105,23 @@ def orienOG(z):
 def elev(x):
     angle = x - 90
     return angle
-# Univector pased on EL & AZ values
-def angle_to_pos(EL, AZ):
+# Unitvector pased on EL & AZ values
+def angle_to_dir(EL, AZ):
     d =  np.pi/180
     x =  np.cos(AZ*d) * np.cos(EL*d)
     y =  np.sin(AZ*d) * np.cos(EL*d)
     z =  np.sin(EL*d)
     vec =  np.array([x, y, z])
     return vec
-# transformed positions into angles
-def pos_to_angle(x,y,z):
+
+# For transforming direction vector into direction angles
+def dir_to_angle(x,y,z):
     AZ_uus = np.arctan(y/x)*180/np.pi + 90
     EL_uus = np.arcsin(z)*180/np.pi
     angles = np.array([EL_uus, AZ_uus])
     return angles
-
-def pos_to_angle2(x,y,z):
+# WARNING! Incorrect approach!!!
+def dir_to_angle2(x,y,z):
     if (x > 0):
         AZ_uus = np.arctan(y/x)*180/np.pi - 90
     else:
@@ -113,7 +129,9 @@ def pos_to_angle2(x,y,z):
     EL_uus = np.arcsin(z)*180/np.pi
     angles = np.array([EL_uus, AZ_uus])
     return angles
-def pos_to_angle3(x,y,z):
+# AZ & EL values are made into a unitvector based on the direction of the East
+# P.S. no longer in use!!!
+def dir_to_angle3(x,y,z):
     if (x > 0):
         AZ_uus = np.degrees(np.arctan(y/x))
     else:
@@ -124,6 +142,29 @@ def pos_to_angle3(x,y,z):
     EL_uus = np.arcsin(z)*180/np.pi
     angles = np.array([EL_uus, AZ_uus])
     return angles
+
+# AZ & EL values are made into a unitvector based on the direction of the North
+def dir_to_angle4(x,y,z):
+    EL_uus = np.degrees(np.arcsin(z))
+    if (x > 0): #x>0, y>0 ning x>0, y<0
+        AZ_uus = 90.0 - np.degrees(np.arctan(y/x))
+    else:
+        if (y > 0):
+            AZ_uus = -90.0 - np.degrees(np.arctan(y/x)) #x<0, y>0
+        else:
+            AZ_uus = -90.0 - np.degrees(np.arctan(y/x)) #x<0, y<0
+    angles = np.array([EL_uus, AZ_uus])
+    return angles
+
+def dir_to_angle5(x,y,z):
+    EL_uus = np.degrees(np.arcsin(z))
+    if (x > 0): #x>0, y>0 ning x>0, y<0
+        AZ_uus = 90.0 - np.degrees(np.arctan(y/x))
+    else:
+        AZ_uus = -90.0 - np.degrees(np.arctan(y/x)) #x<0, y>0 and x<0, y<0
+    angles = np.array([EL_uus, AZ_uus])
+    return angles
+
 # Koordinaatide alguspunt on kuupooduli keskkoht
 # mitte kõige lõunapoolsem jalg
 def centralize(x,y,z):
@@ -136,7 +177,7 @@ def centralize(x,y,z):
 # Algandmete imporimine
 andmed=pd.read_csv("coord_orien_all.csv",header=0,
                    names=['ID','X','Y','Z','RX','RY','RZ'])
-#print('andmed',andmed.head())
+print('Algandmed\n',andmed.head())
 
 #-----------------------------------------------------------------------------
 # RX ja RZ töötlus EL ja AZ andmeteks
@@ -149,8 +190,8 @@ rz = andmed.loc[:, ['RZ']]
 rx = andmed.loc[:, ['RX']]
 #print('Raw RX data:\n',rx.head())
 
-# For testing angle_to_pos
-#print(angle_to_pos(75.800407,69.286656))
+# For testing angle_to_dir
+#print(angle_to_dir(75.800407,69.286656))
 
 # Modifing RX aka EL and RZ aka AZ
 modRX = rx.apply(elev)
@@ -175,38 +216,54 @@ nurgad = pd.concat([modRX, finalRZ], axis=1)
 #nurgad = pd.concat([rx, rz], axis=1)
 print('\nnurgad EL & AZ (from the x-axis):\n',nurgad.head())
 
-# pos is an array of modified RX and RZ values
+# dirANG (direction angles) is an array of modified RX and RZ values
 # form now on RX is true EL and RZ true AZ
-pos = []
+dirANG = []
 for i in np.arange(0, len(nurgad), 1):
-    pos += [angle_to_pos(nurgad['RX'][i], nurgad['RZ'][i])]
-#print("position\n",pos[0:5])  #list of arrays
+    dirANG += [angle_to_dir(nurgad['RX'][i], nurgad['RZ'][i])]
+#print("position\n",dirANG[0:5])  #list of arrays
 
-# modPOS is EL and AZ in a nice dataframe
-modPOS = pd.DataFrame.from_records(pos, columns = list('XYZ'))
-print("\nmodPOS (dir angles EL & AZ made into vectors):\n",modPOS.head())   #arrays in list made into a DataFrame
+# df_dirANG is EL and AZ in a nice dataframe
+df_dirANG = pd.DataFrame.from_records(dirANG, columns = list('XYZ'))
+#print("\ndf_dirANG (dir angles EL & AZ made into vectors):\n",df_dirANG.head())   #arrays in list made into a DataFrame
 
 # Modified positions shall be transformed by the will_turner function
 # into a list of arrays
-trans_pos = []
-for i in np.arange(0, len(modPOS),1):
-    trans_pos += [will_turner(modPOS["X"][i], modPOS["Y"][i], modPOS["Z"][i])]
-#print(trans_pos)
-#print(will_turner(modPOS["X"][0], -modPOS["Y"][0], modPOS["Z"][0]))
+trans_pos_old = []
+for i in np.arange(0, len(df_dirANG),1):
+    trans_pos_old += [will_turner(df_dirANG["X"][i], df_dirANG["Y"][i], df_dirANG["Z"][i])]
+#print(trans_pos_old)
+#print(will_turner(df_dirANG["X"][0], -df_dirANG["Y"][0], df_dirANG["Z"][0]))
+
+# In this section, the function bill_turner is used.
+# bill_turner works in conjuction with the will_turner,
+# but only for the camera direction angles.
+init_dataANG = []
+for i in np.arange(0, len(andmed),1):
+    init_dataANG += [bill_turner(andmed["RX"][i], andmed["RY"][i], andmed["RZ"][i])]
+#print("\nnew transformed direction vector\n",init_dataANG[0:5])
+
+df_setupANG = pd.DataFrame.from_records(init_dataANG, columns=['Xd','Yd','Zd'])
+print("\ndf_setupANG (initial coord turner //Bill// applied to recent vectors):\n",df_setupANG.head())
+
+transfANG = []
+for i in np.arange(0, len(df_setupANG),1):
+    transfANG += [will_turner(df_setupANG["Xd"][i], df_setupANG["Yd"][i], df_setupANG["Zd"][i])]
+#print("\nnew transformed direction vector\n",transfANG[0:5])
 
 # Transformed position coordinates written in a prettier way
 # prettier aka "better to work with"
-newPOS = pd.DataFrame.from_records(trans_pos, columns=['U','V','W'])
-print("\nnewPOS (coord turner applied to recent vectors):\n",newPOS.head())   # pandas DataFrame
+df_newDIRVEC = pd.DataFrame.from_records(transfANG, columns=['U','V','W'])
+print("\ndf_newDIRVEC (coord turner //Will// applied to recent vectors):\n",df_newDIRVEC.head())   # pandas DataFrame
 
 # New angles from new position coordinates
 newANG = []
-for i in range(0, len(newPOS)):
-    newANG += [pos_to_angle3(newPOS["U"][i],newPOS["V"][i],newPOS["W"][i])]
+for i in range(0, len(df_newDIRVEC)):
+    newANG += [dir_to_angle5(df_newDIRVEC["U"][i],df_newDIRVEC["V"][i],df_newDIRVEC["W"][i])]
 #print((newANG[0])[1])   #list of arrays
 
-transANG = pd.DataFrame.from_records(newANG, columns=['EL_uus','AZ_uus'])
-print("\ntransANG (vectors --> EL' & AZ'):\n",transANG.head())
+df_transfDIRANG = pd.DataFrame.from_records(newANG, columns=['EL_uus','AZ_uus'])
+print("\ntransANG (vectors --> EL' & AZ'):\n",df_transfDIRANG.head())
 
 #-----------------------------------------------------------------------------
 # Kaamerate koordinaatide töötlus
@@ -231,7 +288,7 @@ print('\nFirst 5 shifted location coord:\n',corCOORD.head())   #arrays in list m
 #df_graph võtab transformeeritud ja nihutatud kaamerate koordinaadid
 #ning RX, RZ nurkadest moodustatud vektori, mis on samuti transformeeritud
 # ja paneb nad ühte DataFrame'i et
-df_graph = pd.concat([corCOORD,newPOS],axis=1)
+df_graph = pd.concat([corCOORD,df_newDIRVEC],axis=1)
 print('\nAndmed graafiku loomiseks:\n',df_graph.head())
 data = df_graph.to_numpy()
 #print('\nkatse:\n', data)
@@ -399,8 +456,8 @@ ladder_modCoord = pd.DataFrame.from_records(ladder_molder, columns = list('XYZ')
 ladder_shifter = []
 for i in np.arange(0, len(ladder_modCoord), 1):
     ladder_shifter += [centralize(ladder_modCoord['X'][i],
-                                ladder_modCoord['Y'][i],
-                                ladder_modCoord['Z'][i])]
+                                  ladder_modCoord['Y'][i],
+                                  ladder_modCoord['Z'][i])]
 #print(molder)  #again, made into a list of arrays
 
 ladder_corCOORD = pd.DataFrame.from_records(ladder_shifter,
@@ -443,7 +500,7 @@ ax.plot(X_legs[0:3], Y_legs[0:3], Z_legs[0:3], color='orange')
 ax.plot(X_legs[3:6], Y_legs[3:6], Z_legs[3:6], color='orange')
 ax.plot(X_legs[6:9], Y_legs[6:9], Z_legs[6:9], color='orange')
 ax.plot(X_legs[9:12], Y_legs[9:12], Z_legs[9:12], color='orange')
-ax.scatter(X_legs[12:len(legs_df)], Y_legs[12:len(legs_df)], 
+ax.scatter(X_legs[12:len(legs_df)], Y_legs[12:len(legs_df)],
            Z_legs[12:len(legs_df)], color='orange')
 
 # Cylinder
@@ -457,17 +514,17 @@ Yc = np.sqrt(R**2-Xc**2)
 #ax.plot_surface(Xc, Yc, Zc, alpha=0.2)
 #ax.plot_surface(Xc, -Yc, Zc, alpha=0.2)
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+ax.set_xlabel('Distance (m)')
+ax.set_ylabel('Distance (m)')
+ax.set_zlabel('Distance (m)')
 
 #-----------------------------------------------------------------------------
 #  Modling the final data
-df_new = pd.concat([andmed['ID'], corCOORD, transANG], axis=1)
+df_new = pd.concat([andmed['ID'], corCOORD, df_transfDIRANG], axis=1)
 print('\nFirst 5 transformed values:\n',df_new.head(),
       '\nLast 5 transformed values:\n',df_new.tail())
 
-df_new.to_csv(r'FINAL_CALC_test_new_pos_to_ang.csv',
+df_new.to_csv(r'FINAL_CALC_new_turn_mod.csv',
              index = False, header=True)
 
 aeg = time.time() - start_time
